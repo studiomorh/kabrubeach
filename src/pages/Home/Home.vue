062192d16a2a4777dd7be4de3ca1f58fe1301d3a
Lualin Viscardi <lualinviscardi@gmail.com>
2026-08-04 12:06:21 -0300

    inicio

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="min-h-screen bg-zinc-950 flex items-center justify-center">
+        <h1 class="text-6xl font-bold text-white">
+            KABRU BEACH 🌴
+        </h1>
+    </main>
+</template>
